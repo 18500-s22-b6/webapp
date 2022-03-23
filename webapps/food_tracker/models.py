7ec02dede5f0106e4cf612fa866b7f5ae5524171
jaeyoon25 <32021698+jaeyoon25@users.noreply.github.com>
2022-03-23 12:21:39 -0400

    created ItemEntry model

--- webapps/food_tracker/models.py
+++ webapps/food_tracker/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class ItemEntry(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+    return "ItemEntry(id=" + str(self.id) + ", " \
+                      + "name=" + str(self.name) + ")"
