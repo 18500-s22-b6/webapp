6c108afb2084c45207c95070c169ab141419befe
jaeyoon25 <32021698+jaeyoon25@users.noreply.github.com>
2022-03-30 11:18:07 -0400

    Added models from Design pdf

--- webapps/food_tracker/models.py
+++ webapps/food_tracker/models.py
@@ -2,9 +2,40 @@ from django.db import models
 
 # Create your models here.
 
+# Cabinet and Device are synonymous, in case of documentation discrepancy
+class Device(models.Model): 
+    serial_number = models.IntegerField(blank=True, null=True)
+    status = models.IntegerField(blank=True, null=True)
+    owner = models.ForeignKey(User, on_delete=models.PROTECT)
+    name = models.CharField(max_length = 50)
+    most_recent_image = models.ImageField()
+    key = models.CharField(max_length = 50)
+
+    def __str__(self):
+        return "Device(id=" + str(self.serial_number) \
+                      + ", " + "status=" + str(self.status) \
+                      + ", " + "owner=" + str(self.owner) \
+                      + ", " + "name=" + str(self.name) \
+                      + ", " + "key=" + str(self.key) \
+                      + ")"
+
+# Instances of grocery items
 class ItemEntry(models.Model):
+    id = models.IntegerField(blank=True, null=True)
+    location = models.ForeignKey(Device, on_delete=models.PROTECT)
+    type = models.ForeignKey(Category, on_delete=models.PROTECT)
     name = models.CharField(max_length=50)
 
     def __str__(self):
         return "ItemEntry(id=" + str(self.id) + ", " \
                       + "name=" + str(self.name) + ")"
+
+
+
+# General item classes, in case of documentation discrepancy
+class Category(models.Model):
+    id = models.IntegerField(blank=True, null=True)
+    name = models.CharField(max_length=50)
+    user_gen = models.BooleanField()
+    creator = models.ForeignKey(User, on_delete=models.PROTECT)
+    desc_folder = models.CharField(max_length = 200) # extended max len
